082df89c55da71746d014902d72c68fd76b3a54b
sumitBansalk1 <sumit.bansal@indusnet.co.in>
2024-01-08 16:53:17 +0530

    added one new static error

--- src/config/errors.js
+++ src/config/errors.js
@@ -1,3 +1,4 @@
 module.exports = {
-    missingParameter : "Some parameter are missings"
+    missingParameter : "Some parameter are missings",
+    acceptStringBuffer: "Data either be string or buffer"
 };
